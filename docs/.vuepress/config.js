ccc9052ce50e1423257beb4868a16a6a959364cc
付恒 <63717495+100759@users.noreply.github.com>
2025-04-13 07:29:21 +0800

    更新 VuePress 配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "title": "VuePress Site",
+  "description": "A VuePress site",
+  "base": "/",
+  "lang": "zh-CN",
+  "title_template": "%s | VuePress Site",
+  "themeConfig": {
+    "nav": [],
+    "sidebar": {},
+    "lastUpdatedText": "上次更新",
+    "contributorsText": "贡献者",
+    "repo": "",
+    "docsDir": "docs"
+  }
+}
